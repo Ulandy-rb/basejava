e60b25694a36129c71eea4499d271e392afe6c8e
Ulandy <ulandy.rb@gmail.com>
2020-08-17 15:38:25 +0300

    HW1 Реализованы методы save, get, delete, clear, getAll, size в классе ArrayStorage.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,74 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    public static final int STORAGE_LIMIT = 10000;
+
+    Resume[] storage = new Resume[STORAGE_LIMIT];
+    private int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        if (size == STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+        } else {
+            int index = -1;
+            for (int i = 0; i < size; i++) {
+                if (storage[i].uuid.equals(resume.uuid)) {
+                    System.out.println("Resume already exist");
+                    index = i;
+                    break;
+                }
+            }
+            if (index < 0) {
+                storage[size] = resume;
+                size++;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                index = i;
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+                break;
+            }
+        }
+        if (index < 0) {
+            System.out.println("Resume " + uuid + " not exist");
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] newStorage = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            newStorage[i] = storage[i];
+        }
+        return newStorage;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
